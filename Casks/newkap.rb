1dfcb77833d6dbca7949c8df8df6d77ed5f9d367
MuntasirMalek <muntasirmalek@users.noreply.github.com>
2026-03-16 02:12:52 +0600

    Add NewKap cask v4.0.0

---
+++ Casks/newkap.rb
@@ -0,0 +1,25 @@
+cask "newkap" do
+  version "4.0.0"
+
+  on_intel do
+    sha256 "ac380008b7038896008835ff233fa4b5e44b20415d5314f5bac6000cf2cae811"
+    url "https://github.com/MuntasirMalek/NewKap/releases/download/v#{version}/Kap-3.6.0-x64.dmg"
+  end
+
+  on_arm do
+    sha256 "61481ceeb7698652bc037f810f92a15f082b3092789aec236d18597a16323230"
+    url "https://github.com/MuntasirMalek/NewKap/releases/download/v#{version}/Kap-3.6.0-arm64.dmg"
+  end
+
+  name "NewKap"
+  desc "Community-driven screen recorder (Kap fork)"
+  homepage "https://github.com/MuntasirMalek/NewKap"
+
+  app "Kap.app"
+
+  zap trash: [
+    "~/Library/Application Support/Kap",
+    "~/Library/Preferences/com.wulkano.kap.plist",
+    "~/Library/Logs/Kap",
+  ]
+end
